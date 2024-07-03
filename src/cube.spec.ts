56bb3fe0aff7df8181c8742292742e28da2ffe76
Thibault <tbmc33@gmail.com>
2024-07-03 18:15:07 +0200

    tests

--- src/cube.spec.ts
+++ src/cube.spec.ts
@@ -1,20 +1,20 @@
-import { describe, it, expect } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import Cube from './cube';
 
 describe('Cube', function () {
-  it('should serialize a cube to string for a default cube', function () {
+  test('should serialize a cube to string for a default cube', function () {
     const cube = new Cube();
     const dat = cube.asString();
     const exp = 'UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB';
     return expect(cube.asString()).toBe('UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB');
   });
 
-  it('should initiate a cube when provide a String', function () {
+  test('should initiate a cube when provide a String', function () {
     const cube = Cube.fromString('UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB');
     return expect(cube.asString()).toBe('UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB');
   });
 
-  it('should serialize a cube to JSON for a default cube', function () {
+  test('should serialize a cube to JSON for a default cube', function () {
     const cube = new Cube();
 
     const expectedJSON = {
@@ -28,55 +28,55 @@ describe('Cube', function () {
     return expect(cube.toJSON()).toEqual(expectedJSON);
   });
 
-  it('should rotate U face when move U', function () {
+  test('should rotate U face when move U', function () {
     const cube = new Cube();
     cube.move('U');
     return expect(cube.asString()).toBe('UUUUUUUUUBBBRRRRRRRRRFFFFFFDDDDDDDDDFFFLLLLLLLLLBBBBBB');
   });
 
-  it('should rotate cube face when apply a moves sequence', function () {
+  test('should rotate cube face when apply a moves sequence', function () {
     const cube = new Cube();
     cube.move("U R F' L'");
     return expect(cube.asString()).toBe('DURRUFRRRBRBDRBDRBFDDDFFDFFBLLBDBLDLFUUFLLFLLULRUBUUBU');
   });
 
-  it('should rotate cuve face when apply a moves sequence includes additional notation', function () {
+  test('should rotate cuve face when apply a moves sequence includes additional notation', function () {
     const cube = new Cube();
     cube.move("M' u2 z' S");
     return expect(cube.asString()).toBe('LLRUFULLRDLDBLBDRDBBFUUDBBFRRLDBDRRLURUFRFULUBFFUDDBFF');
   });
 
-  it('should resets the cube to the identity cube', function () {
+  test('should resets the cube to the identity cube', function () {
     const cube = new Cube();
     cube.move("U R F' L'");
     cube.identity();
     return expect(cube.asString()).toBe('UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB');
   });
 
-  it('should return true when the cube is solved (default cube)', function () {
+  test('should return true when the cube is solved (default cube)', function () {
     const cube = new Cube();
     return expect(cube.isSolved()).toBe(true);
   });
 
-  it('should return false when the cube is not solved (random cube), and runs without errors in normal time', function () {
+  test('should return false when the cube is not solved (random cube), and runs without errors in normal time', function () {
     const cube = Cube.random();
     return expect(cube.isSolved()).toBe(false);
   });
 
-  it('should return inverse moves', function () {
+  test('should return inverse moves', function () {
     const moves = Cube.inverse("F B' R");
     return expect(moves).toBe("R' B F'");
   });
 
   // ignore because Travis is slow
-  it.skip('should solve a solved cube :) ', function () {
+  test('should solve a solved cube :) ', function () {
     Cube.initSolver();
     const cube = new Cube();
     return expect(cube.solve()).toBe('R L U2 R L F2 R2 U2 R2 F2 R2 U2 F2 L2');
   });
 
   // ignore because Travis is slow
-  it.skip('should return null if no solution is found (maxDepth too low)', function () {
+  test.skip('should return null if no solution is found (maxDepth too low)', function () {
     Cube.initSolver();
     const cube = Cube.random();
     return expect(cube.solve(1)).toBe(null);
---
+++ src/solveUtilFunction.spec.ts
@@ -0,0 +1,12 @@
+﻿import { describe, expect, test } from 'vitest';
+import { rotateLeft } from './solveUtilFunction';
+
+describe('solveUtilFunction', () => {
+  describe('rotateLeft', () => {
+    test('should rotate 4 first values', () => {
+      const data = [1, 2, 3, 4, 5, 6];
+      rotateLeft(data, 1, 3);
+      expect(data).toEqual([1, 3, 4, 2, 5, 6]);
+    });
+  });
+});
